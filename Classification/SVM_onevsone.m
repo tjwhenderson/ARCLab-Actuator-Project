475d7230117a6203d945db0d2d3a36c0886211ca
inari818 <56528033+inari818@users.noreply.github.com>
2020-03-18 13:07:45 -0700

    Add files via upload

---
+++ Classification/SVM_onevsone.m
@@ -0,0 +1,88 @@
+clear all
+close all
+X = GetGoogleSpreadsheet('1dELDmS4YZjsuyiIjc9m4ZyThvj9uYU64wKo6hq8xEJA');
+classes = ["PZT", "DEA", "IPMC", "SMA", "SCP", "SFA", "TSA", "EAP"];
+
+X(1,:) = [];
+X(:,1) = [];
+Y = X(:,1);
+for i = 1:size(X,1)
+    class = X(i,1);
+    onehotlbl = strcmp(class, classes);
+    [~,lbl(i)] = max(onehotlbl);   
+end
+X = str2double(X);
+X(:,1) = [];
+%X = [X(:,2) X(:,3)];
+X = [X(:,2) X(:,3)];
+X = X';
+
+%Normalize the data
+X = log(X);
+%X = exp(X);
+%data_mean = nanmean(X,2);
+%data_std = nanstd(X,[],2);
+%X = bsxfun(@minus,X,nanmean(X,2));
+%X = bsxfun(@rdivide,X,nanstd(X,[],2));
+
+PZT = X(:,lbl== 1)';
+DEA = X(:,lbl == 2)';
+IPMC = X(:,lbl == 3)';
+SMA = X(:,lbl==4)';
+SCP = X(:,lbl==5)';
+SFA = X(:,lbl == 6)';
+TSA = X(:,lbl == 7)';
+EAP = X(:,lbl ==8)';
+%data_mean = nanmean(X,2);
+%data_std = nanstd(X,[],2);
+%X = bsxfun(@minus,X,nanmean(X,2));
+%X = bsxfun(@rdivide,X,nanstd(X,[],2));
+
+
+
+t = templateSVM('Standardize',true,'SaveSupportVectors',true,'KernelFunction','rbf');
+predictorNames = {'stress','strain'};
+responseName = 'ActuatorType';
+classNames = {'PZT', 'DEA', 'IPMC', 'SMA', 'SCP', 'SFA', 'TSA', 'EAP'};
+Mdl = fitcecoc(X',Y,'Learners',t,'ResponseName',responseName,...
+    'PredictorNames',predictorNames,'ClassNames',classNames);
+L = size(Mdl.CodingMatrix,2);
+sv = cell(L,1);
+for j = 1:L
+    SVM = Mdl.BinaryLearners{j};
+    sv{j} = SVM.SupportVectors;
+    sv{j} = sv{j}.*SVM.Sigma + SVM.Mu;
+end
+
+complete = X';
+%gscatter(complete(:,1),complete(:,2),Y);
+
+markers = {'ko','ro','bo','go','co','yo','mo','wo'};
+for j = 1:L
+    figure(j)
+    hold on
+    %gscatter(complete(:,1),complete(:,2),Y);
+    lbl1 = find(Mdl.CodingMatrix(:,j)==1);
+    lbl2 = find(Mdl.CodingMatrix(:,j)==-1);
+    svs = sv{j};
+    plot(svs(:,1),svs(:,2),markers{1},...
+        'MarkerSize',10 + (3-1)*3);
+    type1 = X(:,lbl== lbl1);
+    type2 = X(:,lbl==lbl2);
+    
+    scatter(type1(1,:),type1(2,:),'r');
+    scatter(type2(1,:),type2(2,:),'b');
+    %plot(X(:,lbl==lbl2)','b*');
+    %draw boundaries
+    [xgrid,ygrid] = meshgrid(linspace(-4,8,100),linspace(-3,7,100));
+    [~,scores] = predict(Mdl.BinaryLearners{j},[xgrid(:),ygrid(:)]);
+    
+    contour(xgrid,ygrid,reshape(scores(:,2),size(xgrid)),[0,0],'g');
+    xlabel('strain')
+    ylabel('stress')
+    title("SVM for "+classes(lbl1)+" and "+classes(lbl2))
+    legend(['support vectors',classes(lbl1),classes(lbl2)],'Location','northwest')
+    filename = [num2str(j) '.jpg'];
+    saveas(j,filename)
+    hold off
+end
